ccc991bcb7bb4a8f48a62f196954510bc53b1c94
Daniel Jozsef <daniel.jozsef@gmail.com>
2020-09-21 12:08:55 +0200

    Yea that didn't work

--- jumon.py
+++ jumon.py
@@ -13,7 +13,7 @@ default_fmt_string = '{spell|0,20}.{password_iteration}'
 iterations = 200000
 
 
-def __save_record(key, value):
+def save_record(key, value):
 	try:
 		with dbm.open(file=dbname, flag='c') as db:
 			dbkey = su.cook_inputstring(key)
@@ -23,7 +23,7 @@ def __save_record(key, value):
 		print('Db access failed on save')
 
 
-def __load_record(key):
+def load_record(key):
 	try:
 		with dbm.open(file=dbname, flag='r') as db:
 			dbkey = su.cook_inputstring(key)
@@ -33,7 +33,7 @@ def __load_record(key):
 		return None
 
 
-def __clear_record(key):
+def clear_record(key):
 	try:
 		with dbm.open(file=dbname, flag='w') as db:
 			db.pop(key)
@@ -41,15 +41,15 @@ def __clear_record(key):
 		print('No such record or db access failed on delete')
 
 
-def __get_record_with_init(index):
-	meta = __load_record(index)
+def get_record_with_init(index):
+	meta = load_record(index)
 	if not meta:
 		meta = { 'firstused': time.time() }
-		__save_record(index, meta)
+		save_record(index, meta)
 	return meta
 
 
-def __get_password_iteration(meta):
+def get_password_iteration(meta):
 	return meta.get('password_iteration', 0)
 
 
@@ -61,33 +61,33 @@ class Jumon(object):
 
 	def __call__(self, service, account=''):
 		index = self.passgen.gen_spellstring(service, account, -1)
-		meta = __get_record_with_init(index)
+		meta = get_record_with_init(index)
 		return self.gen_password(service, account, meta)	
 
 	def next_password(self, service, account):
 		index = self.passgen.gen_spellstring(service, account, -1)
-		meta = __get_record_with_init(index)
-		meta['password_iteration'] = __get_password_iteration(meta) + 1
-		__save_record(index, meta)
+		meta = get_record_with_init(index)
+		meta['password_iteration'] = get_password_iteration(meta) + 1
+		save_record(index, meta)
 		return self.gen_password(service, account, meta)
 
 	def set_fmt_string(self, service, account, fmt_string):
 		index = self.passgen.gen_spellstring(service, account, -1)
-		meta = __get_record_with_init(index)
+		meta = get_record_with_init(index)
 		meta['fmt_string'] = fmt_string
-		__save_record(index, meta)
+		save_record(index, meta)
 		return self.gen_password(service, account, meta)
 
 	def clear_meta(self, service, account):
 		index = self.passgen.gen_spellstring(service, account, -1)
-		__clear_record(index)
+		clear_record(index)
 		print('Done.')
 
 	def gen_password(self, service, account, meta):
 		return self.passgen.gen_password(
 			service=service,
 			account=account,
-			password_iteration=__get_password_iteration(meta),
+			password_iteration=get_password_iteration(meta),
 			fmt_string=self.__get_fmt_string(meta)
 		)
 	
